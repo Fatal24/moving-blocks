7dbce6fb26a605e75efcdd47654326467f4075b4
Rayan Nadeem <rayan.nadeem@outlook.com>
2026-01-31 18:25:51 +0000

    added backend_helper classes

--- backend_game.py
+++ backend_game.py
@@ -6,9 +6,7 @@ from backend_helper import Goal, Tile
 MAX_GAME_SIDE_LENGTH = 31
 MIN_GAME_SIDE_LENGTH = 15
 
-global game = []
-
-def iter_game():
+game = []
 
 def play_game(seed:int, *ips:list[list[int]]):
     #initialisation of game
@@ -16,7 +14,10 @@ def play_game(seed:int, *ips:list[list[int]]):
     dirn = seed % 4
     # 0: up, 1: right, 2: down, 3: left, initial dirn (rotates clockwise every turn)
     size = int(15 + (seed % ((MAX_GAME_SIDE_LENGTH - MIN_GAME_SIDE_LENGTH)/2))*2)
-    game = np.zeros((size, size))
+    game = [[Tile()] * size] * size
+    
+    print(game, size)
+
     game[random.randint(2,size//2 - 2)][random.randint(2,size//2 - 2)] = 1
     game[random.randint(size//2 + 2, size - 2)][random.randint(2,size//2 - 2)] = 2
     game[random.randint(2, size // 2 - 2)][random.randint( size//2 + 2, size - 2)] = 3

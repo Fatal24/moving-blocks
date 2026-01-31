dc140933a49355b1086b583aa7ce8d982ef91593
Adyan Shahid <adyan.shahid24@gmail.com>
2026-01-31 18:51:18 +0000

    added images to each class as a property

--- backend_helper.py
+++ backend_helper.py
@@ -1,3 +1,4 @@
+import os.path
 from enum import Enum
 
 class Direction(Enum):
@@ -12,6 +13,7 @@ class Box:
         self.coords = coords
         self.direction = direction
         self.date = date
+        self.img = os.path.join("Assets", "Box")
 
     def move_one_place:
         
@@ -19,17 +21,22 @@ class Box:
 class Goal:
     def __init__(self, player):
         self.player = player
+        self.img = os.path.join("Assets", "Goal.png")
 
 class Tile:
     def __init__(self, direction=Direction.STILL, box=False):
         self.direction = direction
         self.box = box
+        self.base_img = os.path.join("Assets", "Tile_Background.png")
+        self.dirn_img = os.path.join("Assets", "Tile_Direction.png")
+
 
 class Spawner:
     def __init__(self, direction=Direction.NORTH, threshold=5):
         self.direction = direction
         self.spawn_epoch = 0
         self.epoch_threshold = threshold
+        self.img = os.path.join("Assets", "Spawn.png")
     
     def spawn(self, force_spawn=False):
         if force_spawn:

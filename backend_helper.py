b924ebc1ba3790820590d679a20ff11417764b95
Vijay Lehto <vijaylehto@gmail.com>
2026-01-31 19:55:35 +0000

    did some stuff

--- backend_helper.py
+++ backend_helper.py
@@ -13,24 +13,51 @@ class Box:
         self.direction = direction
         self.date = date
 
-    def move_one_place:
-        
-
 class Goal:
     def __init__(self, player):
         self.player = player
 
 class Tile:
-    def __init__(self, direction=Direction.STILL, box=False):
-        self.direction = direction
-        self.box = box
+    def __init__(self, directions=[], lifespan = 5):
+        self.directions = directions
+        if directions == []:
+            self.direction = STILL
+	else:
+            self.direction = directions[0]
+        self.lifespan = lifespan
+        self.i = 0
+
+    def get_direction(self):
+        if self.directions == []:
+            return STILL
+
+        return_dir = self.directions[i]
+        i = (i+1) % len(self.directions)
+        
+        return return_dir
+
+
+    
 
 class Spawner:
-    def __init__(self, direction=Direction.NORTH, threshold=5):
+    def __init__(self, coords, direction=Direction.NORTH, threshold=5):
         self.direction = direction
+        self.date = 0
         self.spawn_epoch = 0
         self.epoch_threshold = threshold
     
+    #return box after initialising
     def spawn(self, force_spawn=False):
-        if force_spawn:
+        successors = {NORTH : EAST, EAST : SOUTH, SOUTH : WEST, WEST : NORTH}
+
+        self.date += 1
+        self.spawn_epoch += 1
+
+	if force_spawn or self.spawn_epoch == self.epoch_threshold:
+            self.direction = successors[self.direction]
+            self.spawn_epoch = 0
+            return Box(coords, self.direction, self.date)
+	
+        return None
+        
 
